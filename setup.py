6c44dfbbe95a335477bbb5312abe77a97377823b
Neal Clark <neal@skiomusic.com>
2015-08-31 17:14:43 -0700

    Fixing typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='Distutils',
   version='1.0',
   description='A lexicographic binary encoding. Encode strings, bytearrays, integers, floats, and lists in a compact binary form preserving lexicographic sorting order.',
-  author=['Greg Ward', 'Neal Clark'];
+  author=['Greg Ward', 'Neal Clark'],
   author_email='neal@skiomusic.com',
   url='https://github.com/nealclark/lexical-binary',
   packages=['distutils', 'distutils.command'],
