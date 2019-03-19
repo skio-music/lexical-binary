94a605f28ad6943cd37eadcc8dd120a3fe9d7d73
Ben Cook <b@bencook.ca>
2019-03-18 18:23:01 -0700

    Add license to setup.py as specified in LICENSE (#1)

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
   description='A lexicographic binary encoding. Encode strings, bytearrays, integers, floats, and lists in a compact binary form preserving lexicographic sorting order.',
   author=['Greg Ward', 'Neal Clark'],
   author_email='neal@skiomusic.com',
-  url='https://github.com/nealclark/lexical-binary',
+  url='https://github.com/skio-music/lexical-binary',
+  license='MIT',
   py_modules=['lexicalbinary'],
 )
